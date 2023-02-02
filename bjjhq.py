d04147e114e7e685783253a363e0f70ce80404da
Prajwal <prajwalmendon@gmail.com>
2023-02-02 13:36:35 -0700

    Add files via upload

---
+++ bjjhq.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+import requests
+from win32com.client import Dispatch
+
+text = "boxing glove"  # Modify this var to search for a specific product
+
+url = "https://www.bjjhq.com"
+
+result = requests.get(url)
+doc = BeautifulSoup(result.text, "html.parser")
+
+product = doc.find_all({'h1': {'class': 'right'}})
+sale = [elm.text for elm in product]
+
+path = r'C:\users\prajwal\desktop\bjjsale.lnk'
+
+if text in sale[1].lower():
+    shell = Dispatch('WScript.Shell')
+    shortcut = shell.CreateShortCut(path)
+    shortcut.Targetpath = url
+    shortcut.save()
+
+# if text in sale[1].lower():
+#     with open(win_path, 'w') as f:
+#         f.write(url)
