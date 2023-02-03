b8838d0a2bf39283aeeb4667a6928e642fa3b458
Prajwal <prajwalmendon@gmail.com>
2023-02-03 10:23:02 -0700

    Update bjjhq.py

--- bjjhq.py
+++ bjjhq.py
@@ -3,6 +3,7 @@ import requests
 from win32com.client import Dispatch
 
 text = "boxing glove"  # Modify this var to search for a specific product
+path = r'C:\users\prajwal\desktop\bjjsale.lnk' # Change path variable to match your preferred path
 
 url = "https://www.bjjhq.com"
 
@@ -11,8 +12,6 @@ doc = BeautifulSoup(result.text, "html.parser")
 
 product = doc.find_all({'h1': {'class': 'right'}})
 sale = [elm.text for elm in product]
-
-path = r'C:\users\prajwal\desktop\bjjsale.lnk'
 
 if text in sale[1].lower():
     shell = Dispatch('WScript.Shell')
