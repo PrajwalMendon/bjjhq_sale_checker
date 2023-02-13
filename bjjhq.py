87c8705478b0560130ae9b648c774a25b207db20
Prajwal <prajwalmendon@gmail.com>
2023-02-13 12:13:27 -0700

    Update bjjhq.py
    added functionality to use a list to search for many desired items

--- bjjhq.py
+++ bjjhq.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 from win32com.client import Dispatch
 
-text = "boxing glove"  # Modify this var to search for a specific product
+items = ["boxing glove", "tombstone"]  # Modify this var to search for a specific product
 path = r'C:\users\prajwal\desktop\bjjsale.lnk' # Change path variable to match your preferred path
 
 url = "https://www.bjjhq.com"
@@ -13,8 +13,9 @@ doc = BeautifulSoup(result.text, "html.parser")
 product = doc.find_all({'h1': {'class': 'right'}})
 sale = [elm.text for elm in product]
 
-if text in sale[1].lower():
-    shell = Dispatch('WScript.Shell')
-    shortcut = shell.CreateShortCut(path)
-    shortcut.Targetpath = url
-    shortcut.save()
+for item in items: 
+    if item in sale[1].lower():
+        shell = Dispatch('WScript.Shell')
+        shortcut = shell.CreateShortCut(path)
+        shortcut.Targetpath = url
+        shortcut.save()
